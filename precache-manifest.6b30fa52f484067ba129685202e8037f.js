e1cdf6a4867f6e7ccd24c53855e9ec4692b5c986
Steven McCombe <smccombe93@gmail.com>
2024-01-23 21:48:51 -0500

    Updates

---
+++ precache-manifest.6b30fa52f484067ba129685202e8037f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78843ae88aa8bb8c73f0be7c527015f",
+    "url": "/osd_builders/index.html"
+  },
+  {
+    "revision": "9ee9232b2a55e899b9c1",
+    "url": "/osd_builders/static/css/2.f24c9f62.chunk.css"
+  },
+  {
+    "revision": "8b2f3d60d332df1a9177",
+    "url": "/osd_builders/static/css/main.38a0370c.chunk.css"
+  },
+  {
+    "revision": "9ee9232b2a55e899b9c1",
+    "url": "/osd_builders/static/js/2.bb5ccfc9.chunk.js"
+  },
+  {
+    "revision": "c94e6321f509aeaadd02e2a5874dc58d",
+    "url": "/osd_builders/static/js/2.bb5ccfc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2f3d60d332df1a9177",
+    "url": "/osd_builders/static/js/main.1a9f4ab0.chunk.js"
+  },
+  {
+    "revision": "396a7494feb4163e580a",
+    "url": "/osd_builders/static/js/runtime-main.35d8d27b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/osd_builders/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "2828f62f557caf64b29c39c6e3f7e87d",
+    "url": "/osd_builders/static/media/s1.2828f62f.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/osd_builders/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/osd_builders/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/osd_builders/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/osd_builders/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "e6cadaebf938a534641d8675327c50a5",
+    "url": "/osd_builders/static/media/slide1.e6cadaeb.jpg"
+  }
+]);
